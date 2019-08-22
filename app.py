3d8e53f25b227b5ca61c9687329342bcddbe46dd
Alex Alessi <aalessi18@hotmail.com>
2019-08-21 20:08:23 -0400

    Added nested for loop for reading excel spreadsheet

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+# library which helps to read excel spreadsheets
 import xlrd
 
+# Find and store excel file
 excelFile = xlrd.open_workbook('cover_letters.xlsx', on_demand = True)
+# Choose which sheet to use inside your excel file
 worksheet = excelFile.sheet_by_name('Sheet1')
 
-print(worksheet.cell(0, 0).value)
+# Set total amount of rows and columns
+rows = worksheet.nrows
+columns = worksheet.ncols
+
+# Define start, end for the range 
+for row in range(1, rows):
+  for column in range(columns):
+    # print values
+    print(worksheet.cell(row, column).value)
