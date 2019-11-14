c31692b79f14531ba937bdf8e4adfb665c708a8b
Alex Alessi <aalessi18@hotmail.com>
2019-11-14 14:50:02 -0500

    Added an exercise for if-else

--- theory/if-else.py
+++ theory/if-else.py
@@ -1,9 +1,31 @@
-if 
+number = input("Choose a number from 1-9")
 
-elif
+numbers = {
+  "one": 1,
+  "two": 2,
+  "three": 3,
+  "four": 4,
+  "five": 5,
+  "six": 6,
+  "seven": 7,
+  "eight": 8,
+  "nine": 9
+}
 
-elif
+if number == numbers["one"]:
+  print(str(number) + " is lonely")
 
-elif
+elif number == numbers["three"]:
+  print(str(number) + " is a nice number")
 
-else
+elif number == numbers["five"]:
+  print(str(number) + " is party")
+
+elif number == numbers["seven"]:
+  print(str(number) + " makes you win at casinos")
+
+elif number == numbers["nine"]:
+  print(str(number) + " might be a little much...")
+
+else:
+  print("number is even: " + str(number))
