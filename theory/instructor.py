b38e30c04b68adb08d7721b1e2ba46c1fc8c750c
Krishna Patel <thekrish360@hotmail.com>
2019-11-21 14:21:31 -0500

    Added instructor file

---
+++ theory/instructor.py
@@ -0,0 +1,7 @@
+classrooms_in_hall_building = ["H435", "H831", "H545", "H450"]
+
+hall_building = {
+    "street_address": "1455 Boul de Maisonneuve Ouest",
+    "floors": 12,
+    "classrooms": classrooms_in_hall_building,
+}
