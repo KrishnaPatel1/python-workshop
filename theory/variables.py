96822670b97dae60331955bbe5d0ed39583411b4
Krishna Patel <thekrish360@hotmail.com>
2019-11-20 22:10:40 -0500

    Added variables in theory and cleaned up code in app.py

---
+++ theory/variables.py
@@ -0,0 +1,29 @@
+x = 4
+print("x =", x)
+
+x = 4.45
+print("x =", x)
+
+x = "snakes"
+print("x =", x)
+
+z = 2
+y = z + 1
+print("z =", z)
+print("y =", y)
+
+print("z is greater than y:", (z > y))
+
+print("z is equal to integer value of 2:", (z == 2))
+
+print("z is equal to the string value of 2:", (z == "2"))
+
+## Challenge 1
+## Print the average between any two numbers
+## You may define variables to hold the two numbers and one variable to hold the answer
+
+## Challenge 2
+## Add two strings together and print the result
+
+## Challenge 3
+## Do whacky things, break the software, and upload your line of code to slido!
