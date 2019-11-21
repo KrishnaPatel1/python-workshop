6fd9389560b068fadd28ba6e0dfc8ef41c044967
Alex Alessi <aalessi18@hotmail.com>
2019-11-21 13:45:57 -0500

    Modified the code for better explanation

--- theory/if-else.py
+++ theory/if-else.py
@@ -1,5 +1,9 @@
 number = input("Choose a number from 1-9")
 
+# Dictionary example
+# Access variables with their keys, and they will return their values
+# key: value
+
 numbers = {
   "one": 1,
   "two": 2,
@@ -19,7 +23,7 @@ elif number == numbers["three"]:
   print(str(number) + " is a nice number")
 
 elif number == numbers["five"]:
-  print(str(number) + " is party")
+  print(str(number) + " is a party")
 
 elif number == numbers["seven"]:
   print(str(number) + " makes you win at casinos")
@@ -29,3 +33,26 @@ elif number == numbers["nine"]:
 
 else:
   print("number is even: " + str(number))
+
+# Alternatively, List Example 
+# Remember, you start accessing values with "0" and not "1"
+
+numbers = [1,2,3,4,5,6,7,8,9]
+
+if number == numbers[0]:
+  print(str(number) + " is lonely... again")
+
+elif number == numbers[2]:
+  print(str(number) + " i know is a nice number, but really?")
+
+elif number == numbers[4]:
+  print(str(number) + " is still a party")
+
+elif number == numbers[6]:
+  print(str(number) + " might not make you win twice")
+
+elif number == numbers[8]:
+  print(str(number) + " is still a lot")
+
+else:
+  print("Won't change the fact that the number is even: " + str(number))
