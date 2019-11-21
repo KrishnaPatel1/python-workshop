ac3d3c8f691291899ea7d25763448b142e72342a
Alex Alessi <aalessi18@hotmail.com>
2019-11-21 14:20:18 -0500

    Modified theory for for loops

--- theory/for-loops.py
+++ theory/for-loops.py
@@ -1,9 +1,40 @@
 # Scenario: print all numbers between 1 and 100
 
+# Declare initial variables to 0
+average = 0
+total = 0
+amountOfNumbersInRange = 0
+
+# Iterate through the loop, 1 to 99
 print("First for loop")
 for number in range(1, 100):
-  print(number)
+  # Add one everytime we run the loop
+  amountOfNumbersInRange = amountOfNumbersInRange + 1
+  # Add number to total every time the for loop runs
+  total = total + number
 
-# print("Second for loop")
-# for number in range(1, 101):
-#   print(number)
+# Computer the average
+average = total/amountOfNumbersInRange
+
+print("There are " + str(amountOfNumbersInRange) + " numbers in the range")
+print("The sum of all numbers is:  " + str(total))
+print("The average is: " + str(average))
+
+# Reset values
+average = 0
+total = 0
+amountOfNumbersInRange = 0
+
+# Second loop, ranging to 101 in order to include 100 (so 1 - 100)
+print("Second for loop")
+for number in range(1, 101):
+  # Add one everytime we run the loop
+  amountOfNumbersInRange = amountOfNumbersInRange + 1
+  # Add number to total every time the for loop runs
+  total = total + number
+
+average = total/amountOfNumbersInRange
+
+print("There are " + str(amountOfNumbersInRange) + " numbers in the range")
+print("The sum of all numbers is:  " + str(total))
+print("The average is: " + str(average))
--- theory/nested-for-loops.py
+++ theory/nested-for-loops.py
@@ -1,4 +1,5 @@
-for firstDimensionNumber in range(1,11):
-  print("First loop number: " + str(firstDimensionNumber))
-  for secondDimensionNumber in range(1,6):
-    print("Second loop number: " + str(secondDimensionNumber))
+# Remember that the last number is not considered, therefore, we finish at 10
+for rowNumber in range(1,11):
+  print("First loop, row number: " + str(rowNumber))
+  for columnNumber in range(1,6):
+    print("Second loop, column number: " + str(columnNumber))
