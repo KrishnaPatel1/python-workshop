18aaaa0b90ecd1a03ddaa3dca3e969e7ecd9e8bd
Krishna Patel <thekrish360@hotmail.com>
2019-11-21 13:48:44 -0500

    Auto stash before merge of "master" and "origin/master"

--- theory/variables.py
+++ theory/variables.py
@@ -1,22 +1,29 @@
 x = 4
 print("x =", x)
+print()
 
 x = 4.45
 print("x =", x)
+print()
 
 x = "snakes"
 print("x =", x)
+print()
 
 z = 2
 y = z + 1
 print("z =", z)
 print("y =", y)
+print()
 
 print("z is greater than y:", (z > y))
+print()
 
 print("z is equal to integer value of 2:", (z == 2))
+print()
 
 print("z is equal to the string value of 2:", (z == "2"))
+print()
 
 ## Challenge 1
 ## Print the average between any two numbers
