145036f859e4046f149201883614538e5e7f1879
Krishna Patel <thekrish360@hotmail.com>
2019-11-23 11:37:31 -0500

    Modified if-else

--- theory/if_else.py
+++ theory/if_else.py
@@ -1,65 +1,23 @@
 number = int(input("Choose a number from 1-9: "))
 
-# Dictionary example
-# Access variables with their keys, and they will return their values
-# key: value
-
-numbers = {
-  "one": 1,
-  "two": 2,
-  "three": 3,
-  "four": 4,
-  "five": 5,
-  "six": 6,
-  "seven": 7,
-  "eight": 8,
-  "nine": 9
-}
-
-if number == numbers["one"]:
+if number == 1:
   print(str(number) + " is lonely")
 
-elif number == numbers["three"]:
+elif number == 3:
   print(str(number) + " is a nice number")
 
-elif number == numbers["five"]:
+elif number == 5:
   print(str(number) + " is a party")
 
-elif number == numbers["seven"]:
+elif number == 7:
   print(str(number) + " makes you win at casinos")
 
-elif number == numbers["nine"]:
+elif number == 9:
   print(str(number) + " might be a little much...")
 
 else:
   print("You didn't a choose a number between 1-9: " + str(number))
-
-
-
-# Alternatively, List Example 
-# Remember, you start accessing values with "0" and not "1"
-
-numbers = [1,2,3,4,5,6,7,8,9]
-
-if number == numbers[0]:
-  print(str(number) + " is lonely... again")
-
-elif number == numbers[2]:
-  print(str(number) + " i know is a nice number, but really?")
-
-elif number == numbers[4]:
-  print(str(number) + " is still a party")
-
-elif number == numbers[6]:
-  print(str(number) + " might not make you win twice")
-
-elif number == numbers[8]:
-  print(str(number) + " is still a lot")
-
-else:
-  print("Shame on you: " + str(number))
-
-
+  
 
 # Challenge 1
 # Determine the grade of a student given the result of a student using following grading scheme
